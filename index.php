9922f54331835253ac660b35e9ee734e386d78d4
Nomore Mudzvova <nomoincfinance@gmail.com>
2024-02-19 22:27:02 +0200

    changed index.html to index.php so that i can include files and easily pull html from other folders

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="inputBoxes/inputBoxes.css">
+        <title>HTML Elements</title>
+    </head>
+
+    <body>
+        
+    </body>
+</html>
