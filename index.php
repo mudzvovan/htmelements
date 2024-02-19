a66473f6c5fa82b3d67d4c5a3e874a221f6d3951
Nomore Mudzvova <nomoincfinance@gmail.com>
2024-02-19 23:50:27 +0200

    finalised animated textbox label css

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 <html lang="en">
     <head>
         <link rel="stylesheet" href="inputBoxes/inputBoxes.css">
+        <link rel="stylesheet" href="style.css">
         <title>HTML Elements</title>
     </head>
 
     <body>
 
          <?php require_once 'inputBoxes/inputBoxes.html'; ?>
-         
+
     </body>
 </html>
